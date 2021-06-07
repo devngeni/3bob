9ced023a3af9db7167cd66c70ef6d73582b4c1c6
Omambia <omambia@Omambias-MacBook-Pro.local>
2021-06-07 19:53:11 +0300

    [chore] adding the ui templates

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -10,8 +10,14 @@
     >
       <div class="overlay"></div>
       <div class="home-content">
-        <div class="row contents">
-          <div class="home-content-left"></div>
+        <div class="row">
+          <div class="home-content-left col-12 col-xl-12">
+            <h1 class="banner__header">Got 3Bob? Get Bitcoin</h1>
+            <p class="banner__left-content banner__size">
+              Buy, Sell, Trade, Invest. Your favorite crypto assets for 3bob or
+              less. No Internet. No Problem. Trade Now. Dial &times;371#
+            </p>
+          </div>
         </div>
       </div>
 
@@ -31,25 +37,40 @@
       data-natural-width="3000"
       data-natural-height="2000"
     >
-      <div>
-        <h1 style="text-align: right; color: #fff; margin-right: 50px">3BOB</h1>
-        <p style="text-align: right; color: #fff; margin-right: 50px">
-          3BOB is an innovative USSD, SMS and PWA trading platform <br />
-          built on the Solana Blockchain by Kenyans and for Kenyans. <br />3BOB
-          aims to bridge the gap between the crypto world and Mpesa<br />
-          thus enabling users to easily deposit<br />
-          and withdraw crypto to Kshs.<br />
-          Building it on the Solana blockchain allows us to minimize the
-          transaction <br />
-          fees to as low as Kshs 3 ($0.03).
-          <br />
-          <br />
-          <nuxt-link
-            class="btn btn-primary text-white"
-            :to="{ name: 'auth-signin' }"
-            >{{ $auth.loggedIn ? 'Dashboard' : `Connect Phone` }}</nuxt-link
+      <div class="row">
+        <div class="col-lg-6 col-sm-12 col-xl-6 col-xxl-12"></div>
+
+        <div class="col-lg-6 col-sm-12 col-xl-6 col-xxl-12">
+          <h1 style="text-align: right; color: #fff; margin-right: 50px">
+            3BOB
+          </h1>
+          <p
+            style="
+              text-align: right;
+              color: #fff;
+              margin-right: 50px;
+              font-size: 21px !important;
+            "
+            class="banner__size"
           >
-        </p>
+            3BOB is an innovative USSD, SMS and PWA trading platform <br />
+            built on the Solana Blockchain by Kenyans and for Kenyans.
+            <br />3BOB aims to bridge the gap between the crypto world and
+            Mpesa<br />
+            thus enabling users to easily deposit<br />
+            and withdraw crypto to Kshs.<br />
+            Building it on the Solana blockchain allows us to minimize the
+            transaction <br />
+            fees to as low as Kshs 3 ($0.03).
+            <br />
+            <br />
+            <nuxt-link
+              class="btn btn-primary text-white"
+              :to="{ name: 'auth-signin' }"
+              >{{ $auth.loggedIn ? 'Dashboard' : `Connect Phone` }}</nuxt-link
+            >
+          </p>
+        </div>
       </div>
     </section>
   </div>
@@ -61,4 +82,44 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.banner__header {
+  padding: 10px;
+  margin-top: -20px !important;
+  margin-bottom: -2px !important;
+  font-size: 55px !important;
+}
+
+.banner__size {
+  font-size: 26px !important;
+  color: #fff;
+  line-height: 38px !important;
+  font-weight: bold;
+  letter-spacing: 1.7px;
+}
+
+.banner__left-content {
+  width: 600px;
+  padding: 10px;
+}
+
+@media only screen and (max-width: 600px) {
+  .banner__header {
+    font-size: 30px !important;
+    width: 100% !important;
+  }
+
+  .banner__left-content {
+    width: 300px important;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 10px;
+    text-align: left !important;
+  }
+
+  .banner__size {
+    font-size: 12px !important;
+    color: #fff;
+  }
+}
+</style>
